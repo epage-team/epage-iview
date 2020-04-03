05015b22e78a947fac075e1679c08f0783cebe8f
chengzi <ttghost@126.com>
2020-04-03 18:21:04 +0800

    fix: 更新datePicker显示及表达值逻辑

--- src/widgets/datePicker/View.vue
+++ src/widgets/datePicker/View.vue
@@ -11,7 +11,7 @@
       :disabled='schema.disabled'
       :placeholder='schema.placeholder'
       :size='schema.size || rootSchema.size'
-      :type='schema.option.type'
+      :type='getType()'
       :format='schema.option.format'
       :value="model[schema.key]"
       @on-change="onDateChange"
@@ -19,6 +19,11 @@
 </template>
 <script>
 import viewExtend from '../../extends/view'
+import { Epage } from 'epage-iview'
+
+const { include } = Epage.helper
+const timeOptions = ['HH:mm:ss', 'HH:mm', 'mm:ss']
+const monthOptions = ['yyyy-MM', 'yyyy/MM']
 
 export default {
   extends: viewExtend,
@@ -41,6 +46,33 @@ export default {
         this.store.updateModel({ [this.schema.key]: newValue })
         this.event('on-change', ...arguments)
       }
+    },
+    hasTime (format) {
+      return !!timeOptions.filter(t => include(format, t)).length
+    },
+    isMonth (format) {
+      return !!monthOptions.filter(t => include(format, t)).length && !include(format, 'dd')
+    },
+    getType () {
+      const { option } = this.schema
+      const { range, format } = option
+
+      let type = 'date'
+
+      if (range) {
+        type = this.hasTime(format) ? 'datetimerange' : 'daterange'
+      } else {
+        if (format === 'yyyy') {
+          type = 'year'
+        } else if (this.isMonth(format)) {
+          type = 'month'
+        } else if (this.hasTime(format)) {
+          type = 'datetime'
+        } else {
+          type = 'date'
+        }
+      }
+      return type
     }
   }
 }
